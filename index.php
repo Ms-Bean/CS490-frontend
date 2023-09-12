3679bf9c193c579e4c26d8b84253e7e0df3a6857
Ms. Bean <spence12310@icloud.com>
2023-09-12 03:43:33 -0400

    Removed localhost address for heroku prod

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
-//This php file is here to fool heroku into 
-//thinking this is a php project
-//It serves no other purpose.
+// This php file is here to fool heroku into 
+// thinking this is a php project so that I
+// can have front-end as a separate repository.
+// It serves no other purpose.
 <?php header('Location: /index.html');?>
