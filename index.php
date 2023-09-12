b854f0d4758413960756b288b92e0bd8c7929785
Ms. Bean <spence12310@icloud.com>
2023-09-12 19:37:01 -0400

    Configured frontend to run on heroku

---
+++ index.php
@@ -0,0 +1,7 @@
+// This php file is here to fool heroku into 
+// thinking this is a php project so that I
+// can have front-end as a separate heroku app,
+// as heroku will not host a project containing
+// only html files.
+// It serves no other purpose.
+<?php header('Location: /index.html');?>
