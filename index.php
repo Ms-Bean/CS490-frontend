fe796bbd333ed2b27a3cd594fd27d47989030152
Ms. Bean <spence12310@icloud.com>
2023-09-13 03:42:15 -0400

    updated message on fool's php file

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 // This php file is here to fool heroku into 
-// thinking this is a php project so that I
-// can have front-end as a separate heroku app,
-// as heroku will not host a project containing
-// only html files.
+// thinking this is a php project, as heroku
+// will not host a site containing only html files 
 // It serves no other purpose.
 <?php header('Location: /index.html');?>
