29b7f11cbb10c02ac9f2985ee215e9cb61fac43a
Ms. Bean <89927037+Ms-Bean@users.noreply.github.com>
2023-09-13 21:46:36 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 // This php file is here to fool heroku into 
 // thinking this is a php project, as heroku
-// does not have a buildpack for static frontends.
+// does not have a buildpack for pure 
+// HTML/CSS/JavaScript frontends.
 // It serves no other purpose
 <?php header('Location: /index.html');?>
